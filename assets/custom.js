f71a34ecdca352940a9d768f1ad2593d6e74144a
pinedance <pinedance@gmail.com>
2019-06-05 11:44:08 +0900

    update ui

--- assets/custom.js
+++ assets/custom.js
@@ -1,7 +1,8 @@
 
 // to apply prism
 (function(){
-  var el = document.getElementsByTagName('code');
+  // var el = document.getElementsByTagName('code');
+  var el = document.getElementsByClassName('highlight');
   // Loop the NodeList through
   for (var i = 0; i < el.length; i++) {
       el[i].classList.add('line-numbers');
